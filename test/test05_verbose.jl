cf3bd18f1c496a17da8ca4a46abe4b195b679f9c
PyDataBlog <pimpfada@gmail.com>
2020-04-03 16:19:12 +0200

    fixed verbose test

--- test/test05_verbose.jl
+++ test/test05_verbose.jl
@@ -11,8 +11,8 @@ using Suppressor
     X = rand(4, 150)
     Random.seed!(2020)
     # Capture output and compare
-    r = @capture_out kmeans(X, 3; n_threads=1, max_iters=1, verbose=true)
-    @test r == "Iteration 1: Jclust = 0.31023197229652544\n"
+    r = @capture_out kmeans(Lloyd(), X, 3; n_threads=1, max_iters=1, verbose=true)
+    @test r == "Iteration 1: Jclust = 46.534795844478815\n"
     
 end
 
