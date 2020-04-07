2d54b7c80f25719b568bc74d56a5030ec354d979
PyDataBlog <pimpfada@gmail.com>
2020-04-07 20:01:40 +0200

    initiated mlj interface port

---
+++ src/mlj_interface.jl
@@ -0,0 +1,13 @@
+# TODO 1: a using MLJModelInterface or import MLJModelInterface statement
+using MLJModelInterface
+using ParallelKMeans
+
+# TODO 2: MLJ-compatible model types and constructors,
+
+# TODO 3: implementation of fit, predict/transform and optionally fitted_params for your models,
+
+# TODO 4: metadata for your package and for each of your models
+
+
+
+
