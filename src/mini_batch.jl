9aca10c8b1a478c8a3dfc86cc216a410ac82b3eb
PyDataBlog <pimpfada@gmail.com>
2021-03-10 12:36:32 +0100

    Minibatch convergence done, metric support left

--- src/mini_batch.jl
+++ src/mini_batch.jl
@@ -13,7 +13,7 @@ MiniBatch() = MiniBatch(100)
 function kmeans!(alg::MiniBatch, X, k;
                  weights = nothing, metric = Euclidean(), n_threads = Threads.nthreads(),
                  k_init = "k-means++", init = nothing, max_iters = 300,
-                 tol = 0, max_no_improvement = 10, verbose = false, rng = Random.GLOBAL_RNG)
+                 tol = eltype(X)(1e-6), max_no_improvement = 10, verbose = false, rng = Random.GLOBAL_RNG)
 
     # Get the type and dimensions of design matrix, X
     T = eltype(X)
@@ -27,15 +27,16 @@ function kmeans!(alg::MiniBatch, X, k;
 
     # Initialize nearest centers
     labels = Vector{Int}(undef, alg.b)
+    final_labels = Vector{Int}(undef, ncol)
 
     converged = false
     niters = 0
+    counter = 0
     J_previous = zero(T)
     J = zero(T)
 
     # TODO: Main Steps. Batch update centroids until convergence
     while niters <= max_iters
-        counter = 0
 
         # b examples picked randomly from X (Step 5 in paper)
         batch_rand_idx = isnothing(weights) ? rand(rng, 1:ncol, alg.b) : wsample(rng, 1:ncol, weights, alg.b)
@@ -79,12 +80,27 @@ function kmeans!(alg::MiniBatch, X, k;
         end
 
         # TODO: Check for early stopping convergence
-        if (niters > 1) & abs(J - J_previous)
+        if (niters > 1) & abs((J - J_previous) < (tol * J))
             counter += 1
 
             # Declare convergence if max_no_improvement criterion is met
             if counter >= max_no_improvement
                 converged = true
+                # TODO: Compute label assignment for the complete dataset
+                @inbounds for i in axes(X, 2)
+                    min_dist = distance(metric, X, centroids, i, 1)
+                    label = 1
+
+                    for j in 2:size(centroids, 2)
+                        dist = distance(metric, X, centroids, i, j)
+                        label = dist < min_dist ? j : label
+                        min_dist = dist < min_dist ? dist : min_dist
+                    end
+
+                    final_labels[i] = label
+                end
+                # TODO: Compute totalcost for the complete dataset
+                J = sum_of_squares(X, final_labels, centroids)  # just a placeholder for now
                 break
             end
 
@@ -94,7 +110,7 @@ function kmeans!(alg::MiniBatch, X, k;
         niters += 1
     end
 
-    return centroids, niters, converged, labels, J  # TODO: push learned artifacts to KmeansResult
+    return centroids, niters, converged, final_labels, J  # TODO: push learned artifacts to KmeansResult
     #return KmeansResult(centroids, containers.labels, T[], Int[], T[], totalcost, niters, converged)
 end
 
